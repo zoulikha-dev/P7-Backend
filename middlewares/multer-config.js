43fac7b3bf56b5d596e5d4650a71b48b83631ee3
zoulikha <zaz13@me.com>
2023-06-26 16:52:26 +0200

    add configuration multer

---
+++ middlewares/multer-config.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+
+// Définition des types MIME pour les extensions d'images acceptées
+const MIME_TYPES = {
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
+};
+
+// Configuration du stockage des fichiers avec Multer
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    // Destination du fichier : le dossier "images"
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    // Génération du nom de fichier unique
+    const name = file.originalname.split(" ").join("_"); // Remplacement des espaces par des underscores
+    const extension = MIME_TYPES[file.mimetype]; // Récupération de l'extension en fonction du type MIME
+    callback(null, name + Date.now() + "." + extension); // Nom du fichier : originalname + timestamp + extension
+  },
+});
+
+// Export du middleware Multer configuré pour traiter les fichiers uniques avec le stockage défini
+module.exports = multer({ storage: storage }).single("image");
