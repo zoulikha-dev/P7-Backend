c12fcb521827c112f3f4bf40b135ec5fe204dad7
zoulikha <zaz13@me.com>
2023-07-04 02:40:09 +0200

    Add route for book rating

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bookCtrl = require("../controllers/book");
-const multer = require("../middlewares/multer-config");
+const auth = require("../middlewares/auth");
+const {
+  multerConfig,
+  deleteOldImage,
+  optimizeImage,
+} = require("../middlewares/multer-config");
 
 const router = express.Router();
 
@@ -11,12 +16,15 @@ router.get("/", auth, bookCtrl.getAllBooks);
 router.get("/:id", auth, bookCtrl.getOneBook);
 
 // Route pour créer un nouveau livre
-router.post("/", auth, multer, bookCtrl.createBook);
+router.post("/", auth, multerConfig, optimizeImage, bookCtrl.createBook);
 
 // Route pour mettre à jour un livre existant
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
+router.put("/:id", auth, multerConfig, deleteOldImage, bookCtrl.modifyBook);
 
 // Route pour supprimer un livre
 router.delete("/:id", auth, bookCtrl.deleteBook);
+
+//Route pour ajouter une notation à un livre
+router.post("/:id/rating", auth, bookCtrl.addRating);
 
 module.exports = router;
